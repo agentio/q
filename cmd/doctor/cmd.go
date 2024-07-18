90e372445ad98169bfedfb8a6469f75706b86151
Tim Burks <timburks@me.com>
2024-07-18 15:43:50 -0700

    q doctor

--- cmd/doctor/cmd.go
+++ cmd/doctor/cmd.go
@@ -1,13 +1,49 @@
 package doctor
 
 import (
+	"fmt"
+
+	"github.com/agentio/q/pkg/gcloud"
 	"github.com/spf13/cobra"
 )
+
+var verbose bool
 
 func Cmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "doctor",
 		Short: "Verify necessary dependencies and configuration",
+		RunE:  action,
 	}
+	cmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "display intermediate information")
 	return cmd
+}
+
+func action(cmd *cobra.Command, args []string) error {
+	cmd.SilenceUsage = true // Let the doctor's recommendations stand out.
+	fmt.Printf("Checking gcloud configuration...\n")
+	info, err := gcloud.GetInfo(verbose)
+	if err != nil {
+		return err
+	}
+	// there must be a logged-in account
+	account, err := info.Account()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("account = %s\n", account)
+	// a project must be set
+	project, err := info.Project()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("project = %s\n", project)
+	// a run region must be set
+	runregion, err := info.RunRegion()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("run/region = %s\n", runregion)
+	fmt.Printf("Everything looks good!\n")
+	return nil
 }
---
+++ pkg/gcloud/package.go
@@ -0,0 +1,70 @@
+package gcloud
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"os/exec"
+)
+
+func GetInfo(verbose bool) (*Info, error) {
+	output, err := exec.Command("gcloud", "info", "--format=json").Output()
+	if err != nil {
+		return nil, err
+	}
+	if verbose {
+		log.Printf("%s", string(output))
+	}
+	info := &Info{}
+	if err = json.Unmarshal(output, info); err != nil {
+		return nil, err
+	}
+	return info, nil
+}
+
+type Info struct {
+	Basic  Basic  `json:"basic"`
+	Config Config `json:"config"`
+}
+
+func (info *Info) Account() (string, error) {
+	if info.Config.Account != "" {
+		return info.Config.Account, nil
+	}
+	return "", errors.New("user required")
+}
+
+func (info *Info) Project() (string, error) {
+	if info.Config.Project != "" {
+		return info.Config.Project, nil
+	}
+	return "", errors.New("project required")
+}
+
+func (info *Info) RunRegion() (string, error) {
+	if info.Config.Properties.Run != nil &&
+		info.Config.Properties.Run["region"] != nil &&
+		info.Config.Properties.Run["region"].Value != "" {
+		return info.Config.Properties.Run["region"].Value, nil
+	}
+	return "", errors.New("run/region required")
+}
+
+type Basic struct {
+	Version string `json:"version"`
+}
+
+type Config struct {
+	Account    string     `json:"account"`
+	Project    string     `json:"project"`
+	Properties Properties `json:"properties"`
+}
+
+type Properties struct {
+	Core map[string]*Property `json:"core"`
+	Run  map[string]*Property `json:"run"`
+}
+
+type Property struct {
+	Value string `json:"value"`
+}
