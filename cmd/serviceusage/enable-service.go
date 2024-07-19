afe41b9bb526d47e79fca69fd4256ae6d30f6e39
Tim Burks <timburks@me.com>
2024-07-18 21:42:26 -0700

    Add more Service Usage function support.

--- cmd/serviceusage/enable-service.go
+++ cmd/serviceusage/enable-service.go
@@ -1,6 +1,13 @@
 package serviceusage
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	serviceusage "cloud.google.com/go/serviceusage/apiv1"
+	"cloud.google.com/go/serviceusage/apiv1/serviceusagepb"
+	"github.com/spf13/cobra"
+	"google.golang.org/protobuf/encoding/protojson"
+)
 
 func enableServiceCmd() *cobra.Command {
 	var output string
@@ -9,7 +16,29 @@ func enableServiceCmd() *cobra.Command {
 		Short: "Enable service",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-
+			ctx := cmd.Context()
+			c, err := serviceusage.NewClient(ctx)
+			if err != nil {
+				return nil
+			}
+			defer c.Close()
+			response, err := c.EnableService(ctx, &serviceusagepb.EnableServiceRequest{
+				Name: args[0],
+			})
+			if err != nil {
+				return err
+			}
+			if output == "json" {
+				metadata, err := response.Metadata()
+				if err != nil {
+					return err
+				}
+				b, err := protojson.Marshal(metadata)
+				if err != nil {
+					return err
+				}
+				fmt.Fprintf(cmd.OutOrStdout(), "%s", string(b))
+			}
 			return nil
 		},
 	}
