872f1a01f6107fc501087b62fae86dd7e7225d8d
Tim Burks <timburks@me.com>
2024-07-20 14:42:10 -0700

    Service compilation progress.

--- cmd/compile/cmd.go
+++ cmd/compile/cmd.go
@@ -5,8 +5,11 @@ import (
 	"log"
 	"os"
 
+	"github.com/agent-kit/q/pkg/compile"
 	"github.com/agent-kit/q/pkg/encoding"
 	"github.com/spf13/cobra"
+	"google.golang.org/genproto/googleapis/api/serviceconfig"
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/descriptorpb"
 )
@@ -17,21 +20,11 @@ func Cmd() *cobra.Command {
 		Short: "Compile a Service Configuration for an API",
 		Args:  cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
-
-			{
-				bytes, err := os.ReadFile(args[0])
-				if err != nil {
-					return err
-				}
-
-				message, err := encoding.UnmarshalYaml(bytes)
-				if err != nil {
-					return err
-				}
-
-				fmt.Fprintf(cmd.OutOrStdout(), "%+v\n", message)
+			config, err := readServiceConfig(args[0])
+			if err != nil {
+				return err
 			}
-
+			// TODO: move to helper function
 			{
 				bytes, err := os.ReadFile(args[1])
 				if err != nil {
@@ -41,9 +34,33 @@ func Cmd() *cobra.Command {
 				if err := proto.Unmarshal(bytes, &descriptors); err != nil {
 					log.Fatalln("Failed to parse descriptors:", err)
 				}
+				compile.AddDetailFromDescriptors(config, &descriptors)
 			}
+			compile.AddCommonEndpointsSettings(config)
+			bytes, err := protojson.Marshal(config)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("%s\n", string(bytes))
 			return nil
 		},
 	}
 	return cmd
+}
+
+func readServiceConfig(filename string) (*serviceconfig.Service, error) {
+	bytes, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	message, err := encoding.UnmarshalYaml(bytes)
+	if err != nil {
+		return nil, err
+	}
+	switch v := message.(type) {
+	case *serviceconfig.Service:
+		return v, nil
+	default:
+		return nil, fmt.Errorf("unsupported type %t", v)
+	}
 }
